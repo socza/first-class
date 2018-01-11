3456a9aef49149a254fd13504ac11c9ba5514a95
Piotr Sociński <psocinski@gmail.com>
2018-01-11 22:25:51 +0100

    Finished task "First Class".

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+	function Phone(brand, price, color, screen) {
+		this.brand = brand;
+		this.price = price;
+		this.color = color;
+		this.screen = screen;
+	}
+
+	Phone.prototype.printInfo = function() {
+	
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	}	
+
+	Phone.prototype.sizeScreen = function() {
+		
+		$("p").append(this.brand + " have screen: " + this.screen + ".\<br\>"); 
+	
+	}
+
+	var iPhone6S = new Phone("Apple", 1339, "silver", "'8");
+	iPhone6S.printInfo();
+	
+	var SamsungGalaxyS6 = new Phone("Samsung", 1193, "white", "'9");
+	SamsungGalaxyS6.printInfo();
+	
+	var OnePlus = new Phone("DigRepair", 300, "black", "'5.6");
+	OnePlus.printInfo();
+	
+	var arrayPhone = [iPhone6S, SamsungGalaxyS6, OnePlus];
+	var newHTML = [];
+
+	for ( var i = 0;  i < arrayPhone.length; i++) {
+
+		newHTML.push(arrayPhone[i].sizeScreen());
+		}
+		$(".element").text(newHTML.join(""));
+
+});
